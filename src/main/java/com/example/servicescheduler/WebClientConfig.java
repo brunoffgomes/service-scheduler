006089da73d453ee91b39658c07bbf173c8e18ed
Bruno Gomes <brunofreiregomes@gmail.com>
2026-03-04 09:15:06 -0300

    Fix WebClient bean wiring (#6)
    Co-authored-by: Bruno Gomes <brunocffg85@gmail.com>

--- src/main/java/com/example/servicescheduler/WebClientConfig.java
+++ src/main/java/com/example/servicescheduler/WebClientConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    WebClient webClient(WebClient.Builder builder) {
-        return builder.build();
+    WebClient webClient() {
+        return WebClient.builder().build();
     }
 }
